b59f981fa74dbe0c046fbcc77d5f247a25718d7c
Sazhin Semen <368749@edu.itmo.ru>
2023-09-23 04:00:40 +0300

    feat: add report module

---
+++ src/Lab1/ModelInfo/ModelInfo.cs
@@ -0,0 +1,16 @@
+namespace Itmo.ObjectOrientedProgramming.Lab1.Model;
+
+// TODO: maybe make records
+// TODO: may be should be static
+public class ModelInfo
+{
+    public bool SuccessfulRoad { get; set; }
+
+    public bool NotEnoughPower { get; set; }
+
+    public bool ShipDestroyed { get; set; }
+
+    public bool PeopleDead { get; set; }
+
+    public int FuelSpent { get; set; }
+}
