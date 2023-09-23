cc4516aa6d681120fb32c51a770a750da9b025eb
Sazhin Semen <368749@edu.itmo.ru>
2023-09-23 03:56:49 +0300

    feat: add metric values

---
+++ src/Lab1/MetricValues/MetricValues.cs
@@ -0,0 +1,23 @@
+namespace Itmo.ObjectOrientedProgramming.Lab1.MetricValues;
+
+public static class MetricValues
+{
+    public static int LowSpeed { get; set; } = 50;
+    public static int MidSpeed { get; set; } = 100;
+    public static int FastSpeed { get; set; } = 150;
+
+    public static int PlasmaPrice { get; set; } = 20;
+    public static int GravitonPrice { get; set; } = 50;
+
+    public static int ShortDistance { get; set; } = 250;
+    public static int MidDistance { get; set; } = 500;
+    public static int LongDistance { get; set; } = 1000;
+
+    public static int LowUsage { get; set; } = 6;
+    public static int MidUsage { get; set; } = 10;
+    public static int HighUsage { get; set; } = 15;
+
+    public static int LowMassRate { get; set; } = 1;
+    public static int MidMassRate { get; set; } = 2;
+    public static int HighMassRate { get; set; } = 3;
+}
