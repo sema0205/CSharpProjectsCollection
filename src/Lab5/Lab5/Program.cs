1dba4b60f32b67970c56da7231095f093f93a7a2
Sazhin Semen <368749@edu.itmo.ru>
2023-12-15 01:25:26 +0300

    feat: add main app point

---
+++ src/Lab5/Lab5/Program.cs
@@ -0,0 +1,35 @@
+﻿using Lab5.Application.Extensions;
+using Lab5.Infrastructure.DataAccess.Extensions;
+using Lab5.Presentation.Console;
+using Lab5.Presentation.Console.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Spectre.Console;
+
+var collection = new ServiceCollection();
+
+collection
+    .AddApplication()
+    .AddInfrastructureDataAccess(configuration =>
+    {
+        configuration.Host = "localhost";
+        configuration.Port = 6432;
+        configuration.Username = "postgres";
+        configuration.Password = "postgres";
+        configuration.Database = "postgres";
+        configuration.SslMode = "Prefer";
+    })
+    .AddPresentationConsole();
+
+ServiceProvider provider = collection.BuildServiceProvider();
+using IServiceScope scope = provider.CreateScope();
+
+scope.UseInfrastructureDataAccess();
+
+ScenarioRunner scenarioRunner = scope.ServiceProvider
+    .GetRequiredService<ScenarioRunner>();
+
+while (true)
+{
+    scenarioRunner.Run();
+    AnsiConsole.Clear();
+}
